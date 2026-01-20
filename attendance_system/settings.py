afbe89b86203d5114b876989e7538e321fc3843a
asalihu06 <asalihu06@gmail.com>
2026-01-20 09:04:01 +0100

    Switch database to Neon PostgreSQL

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -6,6 +6,7 @@ import cloudinary.api
 import cloudinary_storage
 from dotenv import load_dotenv
 import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = BASE_DIR / '.env'
